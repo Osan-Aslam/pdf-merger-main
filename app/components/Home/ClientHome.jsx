550fb7d3c5828101c559d96c3384fc7946ee46fa
Osan Aslam <149369821+Osan-Aslam@users.noreply.github.com>
2025-06-11 07:56:11 -0700

    complete

--- app/components/Home/ClientHome.jsx
+++ app/components/Home/ClientHome.jsx
@@ -1,9 +1,8 @@
-'use client';
 import { useApi } from "../../context/ApiContext";
 import { HomeComponent } from "./page";
 
 export default function ClientHome() {
-	const { apiResponse } = useApi();
+	const { apiResponse, loading } = useApi();
 
 	return (
 		<>
--- app/context/ApiContext.js
+++ app/context/ApiContext.js
@@ -1,29 +1,11 @@
 "use client";
-import { createContext, useContext, useEffect, useState } from "react";
-import axios from "axios";
+import { createContext, useContext, useState } from "react";
 
 const ApiContext = createContext();
 
-export const ApiProvider = ({ children }) => {
-	const [apiResponse, setApiResponse] = useState(null);
-	const [loading, setLoading] = useState(true);
-
-	useEffect(() => {
-		const fetchApi = async () => {
-			try {
-				const response = await axios.get("http://localhost:5089/api/page", {
-					headers: { Accept: "application/json" },
-				});
-				setApiResponse(response.data);
-			} catch (error) {
-				console.error("API fetch error:", error);
-			} finally {
-				setLoading(false);
-			}
-		};
-
-		fetchApi();
-	}, []);
+export const ApiProvider = ({ children, initialData }) => {
+	const [apiResponse] = useState(initialData);
+	const loading = false;
 
 	return (
 		<ApiContext.Provider value={{ apiResponse, loading }}>
